b295b7a7c01e418757bcbf7a5371ea4d58fd6e37
aslihanX <av.aslihangulluoglu@gmail.com>
2026-02-04 12:43:35 +0000

    test(ui): add admin login happy path test

---
+++ ui-tests/pages/AdminPage.ts
@@ -0,0 +1,23 @@
+import { Locator } from "@playwright/test";
+
+export class AdminPage {
+  constructor(private page) {}
+
+  async login(username: string, password: string) {
+    await this.page.getByLabel('Username').fill(username);
+    await this.page.getByLabel('Password').fill(password);
+    await this.page.getByRole('button', { name: 'Login' }).click();
+  }
+
+  logoutButton() {
+    return this.page.getByRole('button', { name: 'Logout' });
+  }
+
+  loginButton() {
+    return this.page.getByRole('button', { name: 'Login' });
+  }
+
+  invalidCredentialsAlert(): Locator {
+    return this.page.locator('.alert.alert-danger', { hasText: /Invalid credentials/i });
+  }
+}
---
+++ ui-tests/tests/admin.spec.ts
@@ -0,0 +1,31 @@
+import { test as base , expect } from '@playwright/test'
+import { AdminPage } from '../pages/AdminPage'
+import { HomePage } from '../pages/HomePage';
+
+const test = base.extend({
+  context: async ({ browser }, use) => {
+    const context = await browser.newContext();
+    await use(context);
+    await context.close();
+  }
+});
+
+test('Admin login and dashboard', async ({ page }) => {
+  const admin = new AdminPage(page)
+
+  await page.goto('/admin')
+  await admin.login('admin', 'password')
+  await expect(admin.logoutButton()).toBeVisible()
+})
+
+test('Admin - redirect & Messages Tab visible', async ({ page }) => {
+  await page.goto('/admin')
+
+  await page.getByLabel('Username').fill('admin')
+  await page.getByLabel('Password').fill('password')
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  await expect(page).toHaveURL(/admin/)
+  await expect(page.getByText('Messages')).toBeVisible()
+})
+
